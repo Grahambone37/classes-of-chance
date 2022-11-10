e6509a8354e475a1e9a3a0dc0e3642f496b6c035
Grahambone37 <electricgrunt37@gmail.com>
2022-11-10 12:48:17 -0800

    first Activity

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 class Casino {
-    // Write code here
+    constructor(name, booli) {
+        this.name = name
+        this.timesPlayed = 0
+        this.isRigged = booli
+    }
+
+    playGame(betAmount) {
+        this.timesPlayed += 1
+        if (Math.random() <= 0.5 || this.isRigged == true) {
+            console.log(`${this.name} Wins.`)
+        } else {
+            console.log(`You Win ${betAmount * (this.timesPlayed + 1)}!!!`)
+            this.timesPlayed = 0
+        }
+    }
+
+    rollDie(betAmount) {
+        this.timesPlayed += 1
+        if (Math.round(Math.random() * 6) > 1) {
+            console.log(`${this.name} Wins.`)
+        } else {
+            console.log(`You Win ${betAmount * (this.timesPlayed + 1)}!!!`)
+        }
+    }
 };
 
 // TESTS
-const myCasino = new Casino("HackerU Casino");
+const myCasino = new Casino("HackerU Casino", false);
 console.log(myCasino);
 myCasino.playGame(5);
 myCasino.playGame(15);
@@ -11,20 +34,20 @@ myCasino.playGame(25);
 myCasino.playGame(35);
 
 // BONUS TESTS
-/*
+
 const myBonusCasino = new Casino("HackerU Bonus Casino", true);
 console.log(myBonusCasino);
 myBonusCasino.playGame(5);
 myBonusCasino.playGame(15);
 myBonusCasino.playGame(25);
 myBonusCasino.playGame(35);
-*/
+
 
 // Extra BONUS TESTS
-/*
+
 const myExtraBonusCasino = new Casino("HackerU Extra Bonus Casino", false);
 console.log(myExtraBonusCasino);
 myExtraBonusCasino.rollDie(6);
 myExtraBonusCasino.rollDie(20);
 myExtraBonusCasino.rollDie(100);
-*/
+
